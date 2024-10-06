6b0a7c6077d0b59b5d609f78c5fd7a72431d3682
Liliana <liliana.ubisoft@gmail.com>
2024-10-06 18:50:27 +0100

    OK

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'));
 const MoviesPage = React.lazy(() => import('./pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = React.lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
 const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+const MovieCast = React.lazy(() => import('./components/MovieCast/MovieCast'));
+const MovieReviews = React.lazy(() => import('./components/MovieReviews/MovieReviews'));
 
 function App() {
   
@@ -17,12 +19,16 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+          
+          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
           <Route path="/404" element={<NotFoundPage />} />
           <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
       </React.Suspense>
-    </> 
+    </>  
         )
 }
 
